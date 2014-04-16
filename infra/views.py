684647d3823781268a1c10d809c95df64655a2e9
Eugene <me@garreth.spb.ru>
2014-04-16 23:35:36 +0400

    Finish search form. Add pagination. Tweak styles.

--- infra/views.py
+++ infra/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.views import generic
+from django.forms import ModelForm
+from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from infra.models import Retailer
+
+# Create your forms
+
+class RetailerSearchForm(ModelForm):
+    name = forms.CharField(required=False)
+    contract_id = forms.CharField(required=False)
+    class Meta:
+        model = Retailer
+        fields = ['name', 'contract_id']
 
 # Create your views here.
 
@@ -12,8 +24,32 @@ def index(request):
 
 def retailer_list(request):
 
-    retailers = Retailer.objects.order_by('id')
-    context = { 'retailers': retailers }
+    query = {}
+    if request.method == 'POST':
+        form = RetailerSearchForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['name']:
+                query['name__iexact'] = form.cleaned_data['name']
+            if form.cleaned_data['contract_id']:
+                query['contract_id__iexact'] = form.cleaned_data['contract_id']
+    else:
+        form = RetailerSearchForm()
+    
+    if query:
+        retailers = Retailer.objects.filter(**query)
+    else:
+        retailers = Retailer.objects.order_by('id')
+
+    paginator = Paginator(retailers, 1)
+    page = request.GET.get('page', 1)
+    try:
+        retailer_list = paginator.page(page)
+    except PageNotAnInteger:
+        retailer_list = paginator.page(1)
+    except EmptyPage:
+        retailer_list = paginator.page(paginator.num_pages)
+
+    context = { 'retailers': retailer_list, 'form': form }
     return render(request, 'infra/index.html', context)
 
 def retailer_details(request, retailer_id):
